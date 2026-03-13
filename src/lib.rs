98e426c40df4afbdec461fd3a994f7e179b00ed2
CinisBorn <zafkielc@gmail.com>
2026-03-13 16:37:30 -0300

    rework in the tree

---
+++ src/lib.rs
@@ -0,0 +1,95 @@
+#[derive(Debug)]
+struct Node<T: Ord + Clone> {
+    element: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl<T: Ord + Clone> Node<T> {
+    fn insert(node: &mut Option<Box<Node<T>>>, element: T) {
+        if let Some(node) = node {
+            match element.cmp(&node.element) {
+                std::cmp::Ordering::Equal => (),
+                std::cmp::Ordering::Greater => Self::insert(&mut node.right, element),
+                std::cmp::Ordering::Less => Self::insert(&mut node.left, element),
+            }
+        } else {
+            *node = Some(Box::new(Node {
+                element,
+                left: None,
+                right: None,
+            }))
+        }
+    }
+
+    fn find_min(node: &Option<Box<Node<T>>>) -> Option<T> {
+        let node = node.as_ref()?;
+
+        match &node.left {
+            Some(_) => Self::find_min(&node.left),
+            None => Some(node.element.clone()),
+        }
+    }
+
+    fn find_max(node: &Option<Box<Node<T>>>) -> Option<T> {
+        let node = node.as_ref()?;
+
+        match &node.right {
+            Some(_) => Self::find_max(&node.right),
+            None => Some(node.element.clone()),
+        }
+    }
+    
+    fn remove(node: &mut Option<Box<Node<T>>>, value: T) -> Option<T> {
+        let node_val = node.as_mut()?;
+        let removed = node_val.element.clone();
+        
+        match value.cmp(&node_val.element) {
+            std::cmp::Ordering::Greater => Self::remove(&mut node_val.right, value),
+            std::cmp::Ordering::Less => Self::remove(&mut node_val.left, value), 
+            std::cmp::Ordering::Equal => {
+                if node_val.left.is_some() && node_val.right.is_some() {
+                    let sucessor = Self::find_min(&node_val.right).unwrap();
+                    
+                    node_val.element = sucessor.clone();
+                    Self::remove(&mut node_val.right, sucessor);
+                } else if node_val.left.is_some() && node_val.right.is_none() {
+                    let child = node_val.left.take();
+                    *node = child
+                } else  {
+                    let child = node_val.right.take();
+                    *node = child
+                } 
+                
+                Some(removed)
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct BST<T: Ord + Clone> {
+    root: Option<Box<Node<T>>>,
+}
+
+impl<T: Ord + Clone> BST<T> {
+    pub fn new() -> Self {
+        Self { root: None }
+    }
+
+    pub fn insert(&mut self, element: T) {
+        Node::insert(&mut self.root, element);
+    }
+
+    pub fn find_min(&self) -> Option<T> {
+        Node::find_min(&self.root)
+    }
+    
+    pub fn find_max(&self) -> Option<T> {
+        Node::find_max(&self.root)
+    }
+    
+    pub fn remove(&mut self, value: T) -> Option<T> {
+        Node::remove(&mut self.root, value)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,17 @@
-use std::cmp;
-
-/// A binary search implementation where the`value` is `i32`
-/// by design. It could also be a generic type. 
-#[derive(Debug)]
-struct BinarySearchTree {
-    value: i32,
-    left: Option<Box<BinarySearchTree>>,
-    right: Option<Box<BinarySearchTree>>,
-}
-
-impl BinarySearchTree {
-    /// Creates a new binary search tree with the specified root. 
-    pub fn with_root(root: i32) -> Self {
-        Self {
-            value: root,
-            left: None,
-            right: None,
-        }
-    }
-    /// Inserts a new node in the tree. The exact position depends on the value
-    /// itself. See more in: 
-
-    /// [Binary Search Tree](https://en.wikipedia.org/wiki/Binary_search_tree)
-    pub fn insert(&mut self, value: i32) {
-        match value.cmp(&self.value) {
-            cmp::Ordering::Equal => (),
-            cmp::Ordering::Greater => {
-                self.right
-                    .get_or_insert_with(|| {
-                        Box::new(Self {
-                            left: None,
-                            right: None,
-                            value,
-                        })
-                    })
-                    .insert(value);
-            }
-            cmp::Ordering::Less => {
-                self.left
-                    .get_or_insert_with(|| {
-                        Box::new(Self {
-                            left: None,
-                            right: None,
-                            value,
-                        })
-                    })
-                    .insert(value);
-            }
-        }
-    }
-    
-    /// Checks if a value exists. 
-    pub fn contains(&self, value: i32) -> bool {
-        match value.cmp(&self.value) {
-            cmp::Ordering::Equal => true,
-            cmp::Ordering::Greater => match &self.right {
-                Some(right) => right.contains(value),
-                None => false,
-            },
-            cmp::Ordering::Less => match &self.left {
-                Some(left) => left.contains(value),
-                None => false,
-            },
-        }
-    }
-    
-    /// Gets the maximum value in the tree. It has a complexity of *O(h)*
-    pub fn find_max(&self) -> Option<i32> {
-        if let Some(right) = &self.right {
-            right.find_max() 
-        } else {
-            Some(self.value)
-        }
-    }
-    /// Gets the minimum value in the tree. It has a complexity of *O(h)*
-    pub fn find_min(&self) -> Option<i32> {
-        if let Some(left) = &self.left {
-            left.find_min() 
-        } else {
-            Some(self.value)
-        }
-    }
-}
+use binary_search_tree::BST;
 
 fn main() {
-    let mut tree = BinarySearchTree::with_root(10);
-
+    let mut tree: BST<i32> = BST::new();
+    
+    tree.insert(10);
+    tree.insert(15);
+    tree.insert(16);
     tree.insert(5);
-    tree.insert(12);
-    tree.insert(12); // ignored
-    tree.insert(13);
-
-    println!("{}", tree.contains(5));
-    println!("{}", tree.contains(30_000));
-    println!("{}", tree.contains(100));
+    tree.insert(3);
     
-    println!("Max: {}", tree.find_max().unwrap());
-    println!("Min: {}", tree.find_min().unwrap());
-    
-    dbg!(tree);
+    println!("Min {}", tree.find_min().unwrap());
+    println!("Max {}", tree.find_max().unwrap());
+    println!("Remove {:?}", tree.remove(1310));
+   
+    dbg!(tree);    
 }
